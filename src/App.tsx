e38983ca1ac9684e8dae31c78ed268db659aaaaf
Yong Wang <yongwang.ai@gmail.com>
2019-11-21 16:39:58 -0500

    test ci.yml

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ const App: React.FC = () => {
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
 					Edit <code>src/App.tsx</code> and save to reload--wow Hot Reloading
+					TEST ci.yml
 				</p>
 				<a
 					className="App-link"
