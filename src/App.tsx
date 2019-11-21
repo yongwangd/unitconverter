66c052fbe3a1419c9a493a95b82226b8dcda1e71
Yong Wang <yongwang.ai@gmail.com>
2019-11-21 17:07:26 -0500

    Develop to merge into master (#3)
    squashed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 const App: React.FC = () => {
 	return (
@@ -9,6 +9,7 @@ const App: React.FC = () => {
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
 					Edit <code>src/App.tsx</code> and save to reload--wow Hot Reloading
+					TEST ci.yml
 				</p>
 				<a
 					className="App-link"
