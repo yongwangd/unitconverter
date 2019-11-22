c68248283ce95caf8a20ad3dea655b788dc2d869
Yong Wang <yongwang.ai@gmail.com>
2019-11-22 15:03:58 -0500

    Updates

---
+++ precache-manifest.c07cb48ec4ba4fad1537c0cc33839250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de1cee1c036df6697c0491a5f769cdf",
+    "url": "/unitconverter/index.html"
+  },
+  {
+    "revision": "32a40e0058da0b2dd266",
+    "url": "/unitconverter/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fb5be2b1fc6245a5536d",
+    "url": "/unitconverter/static/js/2.e1408322.chunk.js"
+  },
+  {
+    "revision": "32a40e0058da0b2dd266",
+    "url": "/unitconverter/static/js/main.bd923786.chunk.js"
+  },
+  {
+    "revision": "986ddebe5254d1d5b14f",
+    "url": "/unitconverter/static/js/runtime-main.d7045d48.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/unitconverter/static/media/logo.25bf045c.svg"
+  }
+]);
